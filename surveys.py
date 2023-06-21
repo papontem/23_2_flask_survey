6784ca479e0b63f5b255b822a343d271d9323ec5
papontemjetix <papontem@gmail.com>
2023-06-20 23:31:57 -0400

    progress up to step 3, finished with step 2 thus far. started with the initialization oof a response list outside of app.routes in app.py, created the root route'/' and the base.html template to be used when we then exetend it with startpage.html that will be rendered in the root.for now static is just an icon and an empty css file.

--- surveys.py
+++ surveys.py
@@ -1,14 +1,23 @@
 class Question:
     """Question on a questionnaire."""
 
-    def __init__(self, question, choices=None, allow_text=False):
-        """Create question (assume Yes/No for choices."""
+    def __init__(self, question, choices=["Yes", "No"], allow_text=False):
+        """Create question (assume Yes/No for choices.
+        PAM: Initializes a Question object.
+        
+        Args:
+            question (str): The question text.
+            choices (list, optional): The list of choices for the question. Defaults to ["Yes", "No"].
+            allow_text (bool, optional): Defaults to False. Indicates whether text comments are allowed for the question. 
+        
+        """
 
-        if not choices:
-            choices = ["Yes", "No"]
+        # PAM: better to just default choices to yes or no in the constructor definition
+        # if not choices:
+        #     choices = ["Yes", "No"] 
 
-        self.question = question
         self.choices = choices
+        self.question = question
         self.allow_text = allow_text
 
 
@@ -16,13 +25,20 @@ class Survey:
     """Questionnaire."""
 
     def __init__(self, title, instructions, questions):
-        """Create questionnaire."""
+        """Create questionnaire.
+        PAM: Initializes a Survey object with a list of questions.
+        
+        Args:
+            title (str): The name of the survey
+            instructions (str): The neccesary information for how the user should complete the survey
+            questions (list): The list of question instances
+        """
 
         self.title = title
         self.instructions = instructions
         self.questions = questions
 
-
+# PAM: Default data objects well use for our flask app.py, 2 instances of Survey with 4 instances of Question each.
 satisfaction_survey = Survey(
     "Customer Satisfaction Survey",
     "Please fill out a survey about your experience with us.",
