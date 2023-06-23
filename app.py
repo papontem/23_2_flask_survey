11a9bba31a6aec24a8a7ec5e337120c0ab8e6838
papontemjetix <papontem@gmail.com>
2023-06-23 01:00:55 -0400

    now Step 6 is trully finished , done progres up to step 7

--- app.py
+++ app.py
@@ -30,33 +30,35 @@ def homepage(): #the function that will be executed when decorator is flagged
 
 @app.route('/questions/<int:question_id>')
 def show_desired_question(question_id):
-    """TODO: Step 3
+    """
+    Renders the current question onto the webpage
+
+    TODO: Step 3 PAM: DONE
     renders questions template for any question from the selected survey.
     Shows a form with the current question, listing choices as radio options, and a text field if a text field is allowed.
     Form's action is a post request to route /answer with the users answer.
-    PAM: DONE
+   
 
-    TODO: Step 6: Protecting Questions
-    Right now, your survey app might be buggy. Once people know the URL structure, it's possible for them to manually go to /questions/3 before they've answered questions 1 and 2. They could also try to go to a question id that doesn't exist, like /questions/7.
-
-    To fix this problem, you can modify your view function for the question show page to look at the number in the URL and make sure it's correct. If not, you should redirect the user to the correct URL.
-
-    For example, if the user has answered one survey question, but then tries to manually enter /questions/4 in the URL bar, you should redirect them to /questions/1.
-
-    Once they've answered all of the questions, trying to access any of the question pages should redirect them to the thank you page.
-
-    PAM: Doing ..... 
+    TODO: Step 6: Protecting Questions PAM: DONE
+    condidtions for preventing users from falling out of line of the surveys progress 
+    
+    TODO: Step 7: Flash Messages
+    Using flash, if the user does try to tinker with the URL and visit questions out of order, flash a message telling them they're trying to access an invalid question as part of your redirect.
     """
 
     # Once they've answered all of the questions, trying to access any of the question pages should redirect them to the thank you page.
     if len(RESPONSES) == len(selected_survey.questions):
-        # getting redirected to questions/thank_you.html after completing survey and not the thank_you route???
+
+        flash(f"You have completed the survey. You can close this page now. You will never be able to restart the survey onece finished, as stated beforehand there is no going back.")
+        
         return redirect("/Thank_you")
 
     # if user tries to go to a question id that doesn't exist, like /questions/7 , PAM: /question/-1? already handled by server, the dash from - turns that -1 into a string which is not the expected integer as we want...
     #  if the user has answered one survey question, but then tries to manually enter /questions/4 in the URL bar, you should redirect them to /questions/1.
-    elif question_id > len(RESPONSES) or question_id > len(selected_survey.questions):
+    elif question_id > len(RESPONSES) or question_id < len(RESPONSES) or question_id > len(selected_survey.questions):
 
+        flash(f"Plase Refrain from trying to skip questions or going to questions by manipulating the url.")
+        
         question = selected_survey.questions[len(RESPONSES)]
         return render_template('question.html', question=question, question_id = len(RESPONSES),selected_survey=selected_survey)
     
@@ -69,10 +71,13 @@ def show_desired_question(question_id):
 
 @app.route('/answer', methods =["POST", "GET"])
 def handle_question_answer():
-    """TODO: Step 4
+    """
+    Redirects user to next questions, saves previous answer to our Responses DB.
+
+    TODO: Step 4 PAM: DONE so far
     Appends users answer to our fake DB object RESPONSES
     then send the user through a redirect to the next question.
-    PAM: DONE so far
+    
     """
     # get the answer from the form data, if text answer was given add it as a tuple to answer element thatll be added to responses with their answer choice.
     # print(request.args["answer"])
@@ -87,7 +92,7 @@ def handle_question_answer():
     TODO: Step 5: 
     When survey is complete there wont be a next question so index will be out of range. directing user to a end of survey page.
 
-    PAM: doing ... done i think
+    PAM: doing ... done i think...
     """
     if next_question_index < len(selected_survey.questions):
         return redirect(f"/questions/{next_question_index}")
